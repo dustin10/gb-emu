ffeea180d587b1b501d0f692edb77ce92d97675b
Dustin Dobervich <ddobervich@gmail.com>
2024-10-28 09:45:43 -0500

    add imgui

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,13 @@ pub mod mem;
 
 use crate::{cart::Cartridge, cpu::Cpu, mem::Mmu};
 
+use imgui::Context;
+use imgui_glow_renderer::{
+    glow::{self, HasContext},
+    AutoRenderer,
+};
+use imgui_sdl2_support::SdlPlatform;
+use sdl2::{event::Event, keyboard::Keycode, video::Window};
 use std::rc::Rc;
 
 /// Contains the memory address the program counter should be set to after the boot screen is
@@ -33,14 +40,110 @@ impl Emulator {
             mmu,
         }
     }
-    /// Runs the emulator. Currently, must be called after [`Emulator::load_cartridge`].
+    /// Runs the emulator.
     pub fn run(&mut self) -> anyhow::Result<()> {
-        // TODO: render boot screen
+        let sdl_context = match sdl2::init() {
+            Err(msg) => anyhow::bail!(msg),
+            Ok(ctx) => ctx,
+        };
+
+        let video_subsystem = match sdl_context.video() {
+            Err(msg) => anyhow::bail!(msg),
+            Ok(video_subsystem) => video_subsystem,
+        };
+
+        #[cfg(target_os = "macos")]
+        video_subsystem
+            .gl_attr()
+            .set_context_profile(sdl2::video::GLProfile::Core);
+
+        #[cfg(target_os = "macos")]
+        video_subsystem.gl_attr().set_context_version(3, 3);
+
+        let window = match video_subsystem
+            .window("GameBoy Emulator", 1024, 768)
+            .allow_highdpi()
+            .opengl()
+            .position_centered()
+            .build()
+        {
+            Err(msg) => anyhow::bail!(msg),
+            Ok(window) => window,
+        };
+
+        let gl_context = window
+            .gl_create_context()
+            .expect("OpenGL context can be created");
+
+        window
+            .gl_make_current(&gl_context)
+            .expect("OpenGL context can be activated");
+
+        window
+            .subsystem()
+            .gl_set_swap_interval(1)
+            .expect("OpenGL swap interval can be set");
+
+        let gl = glow_context(&window);
+
+        let mut imgui = Context::create();
+
+        imgui.set_ini_filename(None);
+        imgui.set_log_filename(None);
+
+        imgui
+            .fonts()
+            .add_font(&[imgui::FontSource::DefaultFontData { config: None }]);
+
+        let mut platform = SdlPlatform::new(&mut imgui);
+        let mut renderer = AutoRenderer::new(gl, &mut imgui).expect("Renderer can be created");
+
+        let mut event_pump = match sdl_context.event_pump() {
+            Err(msg) => anyhow::bail!(msg),
+            Ok(event_pump) => event_pump,
+        };
 
         self.cpu.registers.pc = START_INSTRUCTION;
 
-        loop {
-            self.cpu.step(&mut self.mmu);
+        'main: loop {
+            let _ticks = self.cpu.step(&mut self.mmu);
+
+            for event in event_pump.poll_iter() {
+                platform.handle_event(&mut imgui, &event);
+
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyUp {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => break 'main,
+                    _ => {}
+                }
+            }
+
+            platform.prepare_frame(&mut imgui, &window, &event_pump);
+
+            let ui = imgui.new_frame();
+
+            // replace with emulator UI
+            ui.show_demo_window(&mut true);
+
+            let draw_data = imgui.render();
+
+            unsafe { renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
+            renderer.render(draw_data).expect("scene can be drawn");
+
+            window.gl_swap_window();
         }
+
+        tracing::debug!("exited main loop");
+
+        Ok(())
+    }
+}
+
+fn glow_context(window: &Window) -> glow::Context {
+    unsafe {
+        glow::Context::from_loader_function(|s| window.subsystem().gl_get_proc_address(s) as _)
     }
 }
